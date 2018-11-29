52b5dd8de4452651bd408db40bd7146a49e78b87
aspiringguru <bmatthewtaylor@gmail.com>
2018-11-29 18:11:28 +1000

    updated socket.io version, chat between windows works

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ var io = require('socket.io')(http);
 
 
 app.get('/', function(req, res){
-  //res.send('<h1>Hello world..........</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected.....');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log("msg", msg)
+    io.emit('chat message', msg);
   });
 });
 
